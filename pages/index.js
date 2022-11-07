c3200ac3082cde5ce349efe309bc5b6f813d102c
Nkaka23dev <nkakaeric96@gmail.com>
2022-11-07 15:49:01 +0200

    first commint on dev branch

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
     <>
     <div className="flex h-screen">
     <h1 className="text-3xl text-red-500 m-auto font-bold underline">
-      Cozy design on Main Branch..........
+      Comming soon, we on dev..........
     </h1>
     </div>
     </>
